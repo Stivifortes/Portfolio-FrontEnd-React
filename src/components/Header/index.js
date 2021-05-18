95f345c4905dc116c460046bcc3f90e0f3d1c7a8
Stiven Fortes <stivenfortes96@gmail.com>
2021-05-18 19:02:55 -0100

    About page created

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,24 +1,42 @@
-import React from 'react'
-import './style.css'
-import { Link } from 'react-router-dom'
-import Logo from '../../images/logo.jpg'
+import React from "react";
+import "./style.css";
+import { Link } from "react-router-dom";
+import Logo from "../../images/logoSteve.png";
 
 export default function Header() {
-    return (
-        <header>
-            <nav>
-            <div>
-                <img src={Logo} alt="Logo" width='30' height='30'/>
-            </div>
-            <div>
-                <ul>
-                    <li><Link to="/" className="links">Home</Link></li>
-                    <li><Link to="/about" className="links">About</Link></li>
-                    <li><Link to="/portfolio" className="links">Porfolio</Link></li>
-                    <li><Link to="/login" className="links">Login</Link></li>                    
-                </ul>
-            </div>
-            </nav>
-        </header>
-    )
+  return (
+    <header>
+      <nav>
+        <div>
+          <Link to="/">
+            <img src={Logo} alt="Logo" width="130" />
+          </Link>
+        </div>
+        <div>
+          <ul>
+            <li>
+              <Link to="/" className="links">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/about" className="links">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link to="/portfolio" className="links">
+                Porfolio
+              </Link>
+            </li>
+            <li>
+              <Link to="/login" className="links">
+                Login
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
 }
--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,13 +1,54 @@
-import React from 'react'
-import './style.css'
-import Layout from '../../components/Layout'
+import React from "react";
+import "./style.css";
+import Layout from "../../components/Layout";
 
 export default function About() {
-    return (
-        <Layout>
-            <div className='aboutContainer'>
-            About
+  return (
+    <Layout>
+      <div className="aboutContainer">
+        <section className="container">
+          <div className="hero">
+            <h2>Stiven Fortes - Computer Science</h2>
+            <p>
+              <i>
+                “You can’t connect the dots looking forward; you can only
+                connect them looking backwards. So you have to trust that the
+                dots will somehow connect in your future.”
+              </i>{" "}
+              ― Steve Jobs
+            </p>
+          </div>
+          <div className="callToAction">
+            <button>Contact Me</button>
+          </div>
+        </section>
+        <section className="info">
+          <div>
+            <h3>Personal Info</h3>
+            <div className="personal">
+              <div>+2389817768</div>
+              <div>Mindelo, Cape Verde</div>
+              <div>stivenfortes96@gmail.com</div>
+              <div>https://github.com/stivifortes</div>
             </div>
-        </Layout>
-    )
+          </div>
+          <div>
+            <h3>Education</h3>
+            <div className="education">
+              <div>Computer Science - University of Mindelo</div>
+              <div>React Youtube Tutorial</div>
+              <div>MernStack - Udemy Course</div>
+              <div>High School - ETJV</div>
+            </div>
+          </div>
+        </section>
+        <section className="last">
+          <button>See My Projects</button>
+          <p>
+            Lorem ipsum dolorus ipsal mendium Lorem ipsum dolorus ipsal mendium
+          </p>
+        </section>
+      </div>
+    </Layout>
+  );
 }
