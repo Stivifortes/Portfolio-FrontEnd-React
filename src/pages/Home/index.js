b4625177244011b3b250fdd462113d541bfbc6e6
Stiven <stivenfortes96@gmail.com>
2021-05-18 12:22:38 -0100

    Updated Login

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import './style.css'
 import Layout from '../../components/Layout'
 
 export default function Home() {
+
+    const [api, setApi] = useState('')
+    useEffect(() => {
+        fetch('http://localhost:8080')
+        .then(response => response.json())
+        .then(data => console.log("API:", data.message))
+    }, [])
     return (
         <Layout>
             <div className='homeContainer'>
